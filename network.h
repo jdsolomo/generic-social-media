694ad8138387a0dd03a918d4627e61538ee69e9e
jdsolomo <jdsolomo@usc.edu>
2020-03-02 19:01:19 -0800

    Update with original files

---
+++ network.h
@@ -0,0 +1,30 @@
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include "person.h"
+#include "date.h"
+// You may need to add more libraries 
+#include <stdlib.h>
+class Network{
+
+    private:
+        Person* head;
+        Person* tail;
+        int count; 
+        Person* search(string fname, string lname, string bdate);
+
+    public:
+        Network();
+        Network(string fileName);
+        ~Network();
+        void push_front(Person* newEntry);
+        void push_back(Person* newEntry);
+        bool remove(string fname, string lname, string bdate);
+        void saveDB(string filename);
+        void loadDB(string filename);
+        void printDB();
+        void showMenu();
+};
+
+#endif
